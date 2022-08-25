2e0b13564c29702b18d9dd26e3ba4f60db31f44c
DeepeshDragoneel <deepeshash444@gmail.com>
2022-08-25 16:26:24 +0530

    2fa

--- server/server.js
+++ server/server.js
@@ -1,6 +1,40 @@
+require("dotenv").config();
+
 const express = require("express");
 
+require("./db/ConnectionPool");
+
+const fileUpload = require("express-fileupload");
+const bodyParser = require("body-parser");
+
+const mongoose = require("mongoose");
+
+var morgan = require("morgan");
+
+const cors = require("cors");
+
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
 
-app.listen(8000);
+app.use(require("./routes/teacherRountes"));
+app.use(require("./routes/adminRoutes"));
 
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.enable("trust proxy");
+
+app.use(morgan("method :url :response-time ms"));
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, (e) => {
+    if (e) {
+        console.log(e);
+    } else {
+        console.log(`CONNECTION TO EXPRESS ESTABLISHED on PORT: ${port}`);
+    }
+});
